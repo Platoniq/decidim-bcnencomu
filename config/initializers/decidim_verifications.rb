48bcdc41dad0d335cd530ae97839f9fc39f6a9bc
Ivan Vergés <ivan@platoniq.net>
2020-01-10 15:08:45 +0100

    add direct verficiations: activistes

--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -33,8 +33,11 @@ end
 Decidim::Verifications.register_workflow(:direct_verifications_marti) do |workflow|
   workflow.engine = Decidim::DirectVerifications::Verification::Engine
 end
+Decidim::Verifications.register_workflow(:direct_verifications_activistes) do |workflow|
+  workflow.engine = Decidim::DirectVerifications::Verification::Engine
+end
 
 # We need to tell the plugin to handle this method in addition to the default "Direct verification". Any registered workflow is valid.
 Decidim::DirectVerifications.configure do |config|
-  config.manage_workflows = %w(direct_verifications_vella direct_verifications_eixample direct_verifications_sants direct_verifications_corts direct_verifications_sarria direct_verifications_gracia direct_verifications_horta direct_verifications_barris direct_verifications_andreu direct_verifications_marti)
+  config.manage_workflows = %w(direct_verifications_vella direct_verifications_eixample direct_verifications_sants direct_verifications_corts direct_verifications_sarria direct_verifications_gracia direct_verifications_horta direct_verifications_barris direct_verifications_andreu direct_verifications_marti direct_verifications_activistes)
 end
