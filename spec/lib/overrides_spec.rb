ae40ed956a09a83e79109b938f051e6d5c806673
David Igón <david.igon@riseup.net>
2020-11-30 19:32:51 +0100

    add md5sums for the overriden files in branch 0.21-stable-fixed-message-consultations

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -10,9 +10,9 @@ checksums = [
     package: "decidim-core",
     files: {
       # layouts
-      "/app/views/layouts/decidim/_main_footer.html.erb" => "995cddce875d4a8b49a9997526145799",
+      "/app/views/layouts/decidim/_main_footer.html.erb" => "aa621b251a3b7f22c670eb98e1a77105",
       # views
-      "/app/views/decidim/devise/sessions/new.html.erb" => "1da8569a34bcd014ffb5323c96391837",
+      "/app/views/decidim/devise/sessions/new.html.erb" => "b8cd220c06cd5771b68869fd7ec7aaa7",
     }
   },
   {
@@ -21,13 +21,13 @@ checksums = [
       # views
       "/app/views/decidim/consultations/admin/question_configuration/_form.html.erb" => "0655db0b199a7cb49389f055210a9cdb",
       "/app/views/decidim/consultations/admin/responses/_form.html.erb" => "6846d66395457acdd7d6ec839a49b0ec",
-      "/app/views/decidim/consultations/consultations/_question.html.erb" => "364d7f8370cdbe7ae70c545fff2e21fa",
+      "/app/views/decidim/consultations/consultations/_question.html.erb" => "e0490411ca3af6573cc736b297cbe6c8",
       "/app/views/decidim/consultations/consultations/_regular_questions.html.erb" => "0c5093e02db04da679d92415e2abd937", # todo check upgrade
       "/app/views/decidim/consultations/consultations/show.html.erb" => "84a1569b796f724efa304b9dfc40f68a",  # todo check upgrade
       "/app/views/decidim/consultations/question_multiple_votes/_form.html.erb" => "af610283ce7ee20f5ef786228a263d4a",  # todo check upgrade
       "/app/views/decidim/consultations/question_multiple_votes/_voting_rules.html.erb" => "5290000e36d1508d588218ad3bb8c808",  # todo check upgrade
-      "/app/views/decidim/consultations/questions/_vote_modal.html.erb" => "ae7c38afcc6588a00f8298ea69769da7",  # todo check upgrade
-      "/app/views/decidim/consultations/questions/show.html.erb" => "1608782bab215fb95920fc3e824a8a5a",  # todo check upgrade
+      "/app/views/decidim/consultations/questions/_vote_modal.html.erb" => "b23948e4ed7e0360a09faef326bc3664",  # todo check upgrade
+      "/app/views/decidim/consultations/questions/show.html.erb" => "24b06841ae047c8fde62c50e09d91b9b",  # todo check upgrade
     }
   }
 ]
