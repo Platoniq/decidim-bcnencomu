982752751f33e57eedcd7c964e851b9d694d06bb
Vera Rojman <vrojman@protonmail.com>
2021-01-13 13:24:18 +0100

    Delete duplicate migrations

--- db/migrate/20210113115817_add_embedded_videoconference_to_meetings.decidim_meetings.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# This migration comes from decidim_meetings (originally 20201216112857)
-
-class AddEmbeddedVideoconferenceToMeetings < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_meetings_meetings, :embedded_videoconference, :boolean, default: false
-  end
-end
