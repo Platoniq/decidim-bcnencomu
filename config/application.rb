2d62ef6e14f5dd838f8cf83474aef7c2412136e3
Vera Rojman <vrojman@protonmail.com>
2021-01-13 13:46:31 +0100

    Prevent module removed from tree error

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@
 require_relative "boot"
 
 require "rails/all"
+require_relative "../app/middleware/assemblies_scoper"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
