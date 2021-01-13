ee2dd4d97f0e84a364132e73972ade98f1952090
Vera Rojman <vera@platoniq.net>
2021-01-13 15:58:28 +0100

    Update to latest temp/0.23-stable revision (#22)
    * Update to latest temp/0.23-stable revision
    
    * Add migrations
    
    * Fix migration23 (#23)
    
    * fix old migration order
    
    * Update db/migrate/20190326180310_fix_reference_for_all_resources.decidim.rb
    
    Co-authored-by: Vera Rojman <vera@platoniq.net>
    
    * Delete duplicate migrations
    
    * Prevent module removed from tree error
    
    * Add missing translations for jitsi and registrations
    
    * Add initializer for videoconferences
    
    Co-authored-by: Ivan Vergés <ivan@platoniq.net>

--- db/migrate/20190326180310_fix_reference_for_all_resources.decidim.rb
+++ db/migrate/20190326180310_fix_reference_for_all_resources.decidim.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 # This migration comes from decidim (originally 20180206143340)
-
 class FixReferenceForAllResources < ActiveRecord::Migration[5.1]
   def up
     models = ActiveRecord::Base.descendants.select { |c| c.included_modules.include?(Decidim::HasReference) }
