1bd75981705551aaee53273253bfdff2d9575df0
Ivan Vergés <ivan@platoniq.net>
2021-01-13 13:12:12 +0100

    fix old migration order

--- db/migrate/20190326180310_fix_reference_for_all_resources.decidim.rb
+++ db/migrate/20190326180310_fix_reference_for_all_resources.decidim.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # This migration comes from decidim (originally 20180206143340)
-
+require "byebug"
 class FixReferenceForAllResources < ActiveRecord::Migration[5.1]
   def up
     models = ActiveRecord::Base.descendants.select { |c| c.included_modules.include?(Decidim::HasReference) }
