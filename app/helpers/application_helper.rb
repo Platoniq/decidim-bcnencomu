f179694cb368ecdbf89da50de7601bf5f5e0ba59
David Igón <david.igon@riseup.net>
2021-06-16 18:55:46 +0200

    add new footer for fearless cities keeping the original decidim for the other organizations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+	def fearlesscities_site?
+		return false unless fearlesscities = Rails.application.secrets.fearlesscities
+
+	    fearlesscities.include? current_organization&.host&.to_str
+	end
 end
