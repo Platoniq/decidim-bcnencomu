795f2bc51ca93c3ef10f400de9893f90d1c66662
Vera Rojman <vrojman@protonmail.com>
2021-10-05 20:15:48 +0200

    Update config

--- config/initializers/civicrm_verification.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Decidim::Verifications.register_workflow(:civicrm) do |workflow|
-  workflow.form = "Decidim::Civicrm::Verifications::CivicrmVerification"
-end
-
-ActiveSupport::Notifications.subscribe(/^decidim\.user\.omniauth_registration/) do |_name, data|
-  Decidim::Civicrm::VerificationJob.new.perform(data.to_json)
-end
