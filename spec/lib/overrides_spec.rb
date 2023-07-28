4689600f3e554ce26b36ac619ccfb3fc8c49bade
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-07-28 11:06:02 +0200

    Fix overrides spec

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -80,7 +80,7 @@ checksums = [
     files: {
       # The only change for controllers is the full namespace for the parent class as it didn't resolved it well when it
       # was just ApplicationController
-      "/app/controllers/decidim/direct_verifications/verification/admin/authorizations_controller.rb" => "ec24f7eb75ad7ab298ef13a956873cf6",
+      "/app/controllers/decidim/direct_verifications/verification/admin/authorizations_controller.rb" => "5b713aa72da2ba5e4f0fefa840816004",
       "/app/controllers/decidim/direct_verifications/verification/admin/direct_verifications_controller.rb" => "4f9cef25f72bb5ce88480850bd3f162a",
       "/app/controllers/decidim/direct_verifications/verification/admin/imports_controller.rb" => "477a63f3c749de204ccdc0987cd6b20d",
       "/app/controllers/decidim/direct_verifications/verification/admin/stats_controller.rb" => "a0c4ae48b1372ea5d37aae0112c9c826",
